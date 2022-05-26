6691bbdc2713f561691dcd06b3b1459aa60f6d36
trevortrevor <tom.grime@gmail.com>
2022-05-26 12:11:12 +0100

    deamon definition

--- main.py
+++ main.py
@@ -6,20 +6,38 @@ import paho.mqtt.client as mqtt
 import re
 import netifaces as ni
 import encodings.idna
-import yaml
-with open("config.yaml", 'r') as config:
-    try:
-        configuration = yaml.safe_load(config)
-    except yaml.YAMLError as exc:
-        print(exc)
-bind_if = configuration['interface']
-discovery_topic = configuration['discovery_topic'] + "/"
-base_topic = configuration['base_topic']
-model = configuration['model']    
-interface_ip = ni.ifaddresses(bind_if)[ni.AF_INET][0]['addr']
+import logging
+import signal
 
+run = True
+
+def handler_stop_signals(signum, frame):
+    global run
+    run = False
+
+signal.signal(signal.SIGINT, handler_stop_signals)
+signal.signal(signal.SIGTERM, handler_stop_signals)
+
+from uci import Uci
+u = Uci()
+try:
+    config = u.get("mqttled")
+except UciExceptionNotFound:
+    logging.fatal('No config file found at /etc/config/mqttled')
+    quit()
+    
+        
+broker = config['mqtt']
+ledconfig = config['leds']
+triggersconfig = config['triggers']
+
+bind_if = u.get("network",broker['interface'],"device")
+discovery_topic = broker['discovery'] + "/"
+base_topic = broker['basetopic']
+model = broker['model']    
+interface_ip = ni.ifaddresses(bind_if)[ni.AF_INET][0]['addr']
 hostidentifier = os.uname()
-sub_topic = hostidentifier.nodename
+sub_topic = broker['subtopic']
 topic = base_topic + "/" + sub_topic + "/"
 
 device = {
@@ -31,19 +49,21 @@ device = {
 
 def parseTrigger(triggerfile):
     triggers = triggerfile.split()
+    retTriggers = []
     for i, trigger in enumerate(triggers):
         if trigger[0] == "[":
             triggers[i] = trigger.strip('[]')
             currentTrigger = trigger.strip('[]')
-            break
-    return currentTrigger, triggers
+        if trigger in triggersconfig:
+            retTriggers.append(trigger)
+    return currentTrigger, retTriggers
 
 class led(object):
     def __init__(self, id):
         self.id = re.sub('[^A-Za-z0-9]+', '', id)
         self.path = "/sys/class/leds/" + id +"/"
         with open(self.path + "brightness") as f:
-            self.brightness = f.read().rstrip()
+            self.brightness = int(f.read().rstrip())
         with open(self.path + "trigger") as f:
             self.current_trigger, self.triggers = parseTrigger(f.read().rstrip())
         if self.current_trigger == 'none':
@@ -87,13 +107,15 @@ class led(object):
         try:
             command = msg['state']
         except:
-            print("Unknown command message")    
+            logging.error("Unknown command message")    
         try:
             on_mode = msg['effect']
         except KeyError:
-            print("no onmode sepecified")
+            logging.info("no on_mode sepecified")
             on_mode = 'default-on'
-        if command == 'ON':
+        if on_mode == "none":
+            self.turn_off()
+        elif command == 'ON':
             self.turn_on(on_mode)
         elif command == 'OFF':
             self.turn_off()
@@ -102,34 +124,49 @@ class led(object):
     def publish_update(self):
         client.publish(self.stateTopic,self.json_state())
 
-leds = eval(str(os.listdir("/sys/class/leds")))
-leds = {x:led(x) for x in leds}    
+if ledconfig['includeall'] == '1':
+    leds = eval(str(os.listdir("/sys/class/leds")))
+else:
+    leds = ledconfig['include']
     
+
+leds = {x:led(x) for x in leds}    
+for entry in ledconfig['exclude']:
+    del leds[entry]
+       
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
-        print("connected to mqtt")
+        logging.info("MQTTLED has connected to the broker")
     else:
-        print("Failed to connect with rc:" + rc)
+        logging.fatal("MQTTLED Failed to connect with rc:" + rc)
+        run = False
     client.subscribe(base_topic + "/#")
-    client.publish(base_topic + "/" + hostidentifier.nodename + "/connection", json.dumps({"state": "on"}))
+    client.publish(base_topic + "/" + hostidentifier.nodename + "/connection", 'online', retain=True)
+    
 
 def unhandled_message(client, userdata, msg):
-    print("Unknown message")
-    print(str(msg.topic))
-    print(str(msg.payload))
+    logging.info("Unknown message")
+    logging.info(str(msg.topic))
+    logging.info(str(msg.payload))
     
-print(interface_ip)
+logging.info("Binding MQTT Client to "+ interface_ip)
       
 client = mqtt.Client(hostidentifier.nodename)
 client.on_connect = on_connect
 client.username_pw_set('tom', 'wA8ru1pe')
+client.will_set(base_topic + "/" + hostidentifier.nodename + "/connection", 'offline', retain=True)
 client.connect("192.168.1.5", bind_address=str(interface_ip))
 client.message_callback = unhandled_message
 for light in leds.values():
     client.message_callback_add(light.commandTopic, light.on_message)
 client.loop_start()   
 for light in leds.values():
-    client.publish(discovery_topic + "light/" + hostidentifier.nodename + "_" + light.id + "/light/config", light.discoveryPayload)
+    client.publish(discovery_topic + "light/" + hostidentifier.nodename + "/" + light.id + "/config", light.discoveryPayload)
     light.publish_update()
-while True:
+while run:
     pass
+
+client.publish(base_topic + "/" + hostidentifier.nodename + "/connection", 'offline', retain=True)
+client.disconnect()
+client.loop_stop()
+logging.info('MQTTLED has shutdown')
